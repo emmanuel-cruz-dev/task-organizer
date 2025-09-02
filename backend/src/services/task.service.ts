77e2088ed63f59d7d24f36ee33fcc8f979a72fc6
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-02 15:19:03 -0300

    feat: adds task.service.ts to backend/src/services/

---
+++ backend/src/services/task.service.ts
@@ -0,0 +1,33 @@
+import Task, { ITask } from "../models/task.model";
+
+const getAllTasks = async (): Promise<ITask[]> => {
+  return await Task.find();
+};
+
+const getTaskById = async (id: string): Promise<ITask | null> => {
+  return await Task.findById(id);
+};
+
+const createTask = async (taskData: ITask): Promise<ITask> => {
+  const task = new Task(taskData);
+  return await task.save();
+};
+
+const updateTask = async (
+  id: string,
+  taskData: ITask
+): Promise<ITask | null> => {
+  return await Task.findByIdAndUpdate(id, taskData, { new: true });
+};
+
+const deleteTask = async (id: string): Promise<ITask | null> => {
+  return await Task.findByIdAndDelete(id);
+};
+
+export default {
+  getAllTasks,
+  getTaskById,
+  createTask,
+  updateTask,
+  deleteTask,
+};
