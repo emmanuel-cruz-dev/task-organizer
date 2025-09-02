d49a00ecb58bc11717cc88390745b8dc23d9bbf2
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-02 16:20:59 -0300

    feat: adds timestamps to task.model in backend/src/models/

--- backend/src/models/task.model.ts
+++ backend/src/models/task.model.ts
@@ -1,30 +1,27 @@
 import mongoose, { Schema } from "mongoose";
 import { ITask } from "../types/types";
 
-const TaskSchema: Schema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const TaskSchema: Schema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    // createdBy: {
+    //   type: String,
+    //   required: true,
+    // },
   },
-  description: {
-    type: String,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-  // createdBy: {
-  //   type: String,
-  //   required: true,
-  // },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model<ITask>("Task", TaskSchema, "Tasks");
