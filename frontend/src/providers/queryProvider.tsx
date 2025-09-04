6d1c35868aa207233c07f408327effefaff10025
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-04 15:59:27 -0300

    refactor: renames query-client and query-provider to queryClient and queryProvider files

--- frontend/src/providers/queryProvider.tsx
+++ frontend/src/providers/queryProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "@/lib/query-client";
+import { queryClient } from "@/lib/queryClient";
 import { QueryProviderProps } from "@/types/provider";
 
 export function QueryProvider({ children }: QueryProviderProps) {
