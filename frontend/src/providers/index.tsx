63a0d2bd6e98be62f69751a2a150bc5e89a47c7d
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-04 15:58:44 -0300

    refactor: updates queryProvider imports in frontend/src/providers/index.tsx

--- frontend/src/providers/index.tsx
+++ frontend/src/providers/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ProvidersProps } from "@/types/provider";
-import { QueryProvider } from "./query-provider";
+import { QueryProvider } from "./queryProvider";
 
 export function Providers({ children }: ProvidersProps) {
   return <QueryProvider>{children}</QueryProvider>;
